f82adfec2e0c59660ae566d4a8227487c27a8308
steven28zhang <stephenxianweizhang@gmail.com>
2013-08-08 22:33:03 +0800

    init
    v0.01

---
+++ src/com/sxw/itl/ITTechlifeVV.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sxw.itl;
+
+/**
+ * @author Stephen
+ *
+ */
+public final class ITTechlifeVV {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
