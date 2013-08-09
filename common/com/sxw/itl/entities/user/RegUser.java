85cb23e0997492792b334a2e8dacf6335e263f27
steven28zhang <stephenxianweizhang@gmail.com>
2013-08-09 02:37:32 -0700

    2013-08-09

---
+++ common/com/sxw/itl/entities/user/RegUser.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.sxw.itl.entities.user;
+
+/**
+ * @author stephenxianweizhang@gmail.com
+ * 
+ */
+public class RegUser {
+
+	private String userName;
+	private String password;
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
