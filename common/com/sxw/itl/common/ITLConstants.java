831747846edc2f7fce7cb99867437e86c25ee06b
steven28zhang <stephenxianweizhang@gmail.com>
2013-08-09 02:36:13 -0700

    2013-08-09

---
+++ common/com/sxw/itl/common/ITLConstants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.sxw.itl.common;
+
+/**
+ * @author stephenxianweizhang@gmail.com
+ *
+ */
+public class ITLConstants {
+
+	public interface EncrptionType {
+		public static final String MDFIVE = "MD5";
+	}
+}
